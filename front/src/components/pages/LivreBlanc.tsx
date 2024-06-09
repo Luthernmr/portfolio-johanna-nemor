32556062b71da16fd2196be395b760e49be67c2b
Luthernmr <nemorluther@gmail.com>
2024-06-09 18:57:36 +0200

    push

--- front/src/components/pages/LivreBlanc.tsx
+++ front/src/components/pages/LivreBlanc.tsx
@@ -1,10 +1,25 @@
-import { Center, Heading } from "@chakra-ui/react";
+import { Center } from '@chakra-ui/react';
+import { Component } from 'react';
 
-export default function LivreBlanc()
-{
-    return (
-            <Center height={"100vh"}>
-                <Heading fontFamily={'century-gothic'} >Livre blanc en cours de redaction</Heading>
+class LivreBlanc extends Component {
+    componentDidMount() {
+        if (!document.querySelector(`script[data-uid="23eb9de9d6"]`)) {
+            const script = document.createElement("script");
+            script.async = true;
+            script.setAttribute("data-uid", "23eb9de9d6");
+            script.src = "https://nemor.ck.page/23eb9de9d6/index.js";
+            document.getElementById("form-container").appendChild(script);
+        }
+    }
+
+    render() {
+        console.log("Rendering LivreBlanc component");
+        return (
+            <Center p={10}>
+                <div id="form-container"></div>
             </Center>
-    )
+        );
+    }
 }
+
+export default LivreBlanc;
